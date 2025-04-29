bef3fc8b97608f1842a3f8d3041096dfd53dfb74
krislee030 <149238605+krislee030@users.noreply.github.com>
2025-04-29 11:59:41 +0200

    Delete 2_database/archive directory

--- 2_database/archive/adzuna_category_table.sql
+++
@@ -1,9 +0,0 @@
-job=# -- Drop Table if perviously created
-DROP TABLE IF EXISTS categories;
--- Create Table
-CREATE TABLE categories (
-    id SERIAL PRIMARY KEY,
-    tag TEXT NOT NULL,
-    class TEXT NOT NULL,
-    label TEXT NOT NULL
-);
