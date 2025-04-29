b0d62006075765f981c719830d59b6bafea09e23
krislee030 <149238605+krislee030@users.noreply.github.com>
2025-04-29 11:59:24 +0200

    Delete 1_data_collection/api_calls/archive directory

--- 1_data_collection/api_calls/archive/adzuna_salary_csv_convert.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-# Load the Excel file
-df = pd.read_excel("data_collection/output_files/adzuna_salary_24M.xlsx", engine="openpyxl")
-
-# Save it as a CSV file (without the index column)
-df.to_csv("data_collection/output_files/adzuna_salary_24M.csv", index=False)
-
-print("Excel file converted to CSV successfully!")
