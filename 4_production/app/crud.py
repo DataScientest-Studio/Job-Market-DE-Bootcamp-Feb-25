c84405f796c88c177cde7c716634d1093f6436da
Kris <krishybb@yahoo.de>
2025-05-12 14:00:46 +0200

    fixed fast-api table size - all data available now

--- 4_production/app/crud.py
+++ 4_production/app/crud.py
@@ -1,7 +1,27 @@
 # Contains functions that perform database operations
 from sqlalchemy.orm import Session
 from app.models import AdzunaAd, AdzunaSalary, AdzunaCategory
+from typing import Optional
 
+def get_ads(db: Session, skip: int = 0, limit: Optional[int] = None):
+    query = db.query(AdzunaAd).offset(skip)
+    if limit is not None:
+        query = query.limit(limit)
+    return query.all()
+
+def get_salary(db: Session, skip: int = 0, limit: Optional[int] = None):
+    query = db.query(AdzunaSalary).offset(skip)
+    if limit is not None:
+        query = query.limit(limit)
+    return query.all()
+
+def get_category(db: Session, skip: int = 0, limit: Optional[int] = None):
+    query = db.query(AdzunaCategory).offset(skip)
+    if limit is not None:
+        query = query.limit(limit)
+    return query.all()
+
+'''
 def get_ads(db: Session, skip: int = 0, limit: int = 100):
     return db.query(AdzunaAd).offset(skip).limit(limit).all()
 
@@ -10,4 +30,4 @@ def get_salary(db: Session, skip: int = 0, limit: int = 100):
 
 def get_category(db: Session, skip: int = 0, limit: int = 100):
     return db.query(AdzunaCategory).offset(skip).limit(limit).all()
-
+'''
--- 4_production/app/main.py
+++ 4_production/app/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI, Depends
 from sqlalchemy.orm import Session
+from typing import Optional
 from app import models, schemas, crud
 from app.database import SessionLocal, engine
 from app.schemas import AdzunaAdSchema, AdzunaSalarySchema, AdzunaCategorySchema
@@ -16,16 +17,16 @@ def get_db():
         db.close()
 
 @app.get("/ads/", response_model=list[schemas.AdzunaAdSchema])
-def read_ads(skip: int = 0, limit: int = 10, db: Session = Depends(get_db)):
+def read_ads(skip: int = 0, limit: Optional[int] = None, db: Session = Depends(get_db)):
     ads = crud.get_ads(db, skip=skip, limit=limit)
     return ads
 
 @app.get("/salary/", response_model=list[schemas.AdzunaSalarySchema])
-def read_salary(skip: int = 0, limit: int = 10, db: Session = Depends(get_db)):
+def read_salary(skip: int = 0, limit: Optional[int] = None, db: Session = Depends(get_db)):
     ads = crud.get_salary(db, skip=skip, limit=limit)
     return ads
 
 @app.get("/category/", response_model=list[schemas.AdzunaCategorySchema])
-def read_salary(skip: int = 0, limit: int = 10, db: Session = Depends(get_db)):
+def read_salary(skip: int = 0, limit: Optional[int] = None, db: Session = Depends(get_db)):
     ads = crud.get_category(db, skip=skip, limit=limit)
     return ads
